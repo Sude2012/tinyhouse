4c3b2e60400cc304e676baba163054f0bd4e9b11
Selver Sude Özdemir <selversudeozdemir@gmail.com>
2025-06-01 14:59:01 +0300

    admin ve ev sahibi ekranı güncellendi

--- frontend/src/app/admin/page.js
+++ frontend/src/app/admin/page.js
@@ -2,7 +2,7 @@ import AdminDashboard from "../../components/admin/AdminDashboard.js";
 
 export default function AdminLoginPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-5">
       <AdminDashboard />
     </div>
   );
